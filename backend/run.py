49d67ca83a502e7d9f4bc4e4b9cc642e3113b9ad
huanghe <huangh@owltech.cn>
2026-03-12 15:39:53 +0800

    feat: 后端支持PORT环境变量，调整workers为2

--- backend/run.py
+++ backend/run.py
@@ -7,11 +7,13 @@ if __name__ == "__main__":
     # 确保在正确的目录中运行
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
     
+    port = int(os.environ.get("PORT", 8000))
+    
     uvicorn.run(
         "app.main:app",
         host="127.0.0.1",
-        port=8000,
+        port=port,
         reload=False,  # 多进程模式下不支持reload
         log_level="info",
-        workers=multiprocessing.cpu_count() * 2  # CPU核心数的2倍，最大化并发能力
+        workers= 2  # CPU核心数的2倍，最大化并发能力
     )
